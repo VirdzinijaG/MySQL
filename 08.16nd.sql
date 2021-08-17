1f2b8eb5be58adea1107d5698de8b07afc533967
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-17 13:58:24 +0300

    Update 08.16nd.sql

--- 08.16nd.sql
+++ 08.16nd.sql
@@ -1,1 +1,8 @@
 -- select'as: valstybiu sarasas(pavadinimas, kontinentas) , su daugiau nei viena oficiala kalba
+
+select country.name as salis, country.Continent as zemynas, count(*)
+from country, countrylanguage
+where countrylanguage.IsOfficial = 'T' and Country.Code = countrylanguage.CountryCode
+group by CountryCode
+having count(*) >= 2
+order by country.name
