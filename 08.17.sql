49e841c6ff47f5329f2b4e0efe8cd06331663dc0
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-18 09:52:48 +0300

    Update 08.17.sql

--- 08.17.sql
+++ 08.17.sql
@@ -1,3 +1,6 @@
 select *
 from city 
-where countrycode = 'LTU';
+where countrycode = 'LTU'
+
+-- limit 2 rodomi du irasai
+-- limit 3, 2 su dviem parametrais. pirmas - kiek eiluciu praleisti, antras - kiek eiluciu rodyti
