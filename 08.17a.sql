cdc2cdf6d8bf91bef236954fc59d40e7e21ccfa7
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-18 10:36:24 +0300

    Update 08.17a.sql

--- 08.17a.sql
+++ 08.17a.sql
@@ -1,5 +1,7 @@
--- insert into city
--- values (5000, 'Balbieriskis', 'LTU', 'Prienu', 966);
+-- insert into city pridedamas irasasa i city lentele
+-- values (5000, 'Balbieriskis', 'LTU', 'Prienu', 966); 
+-- values turi buti surasytos tokia tvarka, kokia yra lenteleje 
+-- ID Name CountryCode District Population
 
 insert into city (countrycode, district, name, population)
 values ('LTU', 'Jurbarko', 'Klausuciai', 902);
