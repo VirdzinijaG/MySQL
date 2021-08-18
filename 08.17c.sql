d5e926fbff03518d2e5a8c9d79c448d44c650587
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-18 11:20:44 +0300

    Update 08.17c.sql

--- 08.17c.sql
+++ 08.17c.sql
@@ -3,9 +3,15 @@
 -- where id = 5000;
 -- atnaujinamas vienas irasas pagal id
 
-update city
-set population = population + 15
-where countrycode = 'LTU';
+-- update city
+-- set population = population + 15
+-- where countrycode = 'LTU';
 -- atnaujinami irasai pagal countryde ir visiems padidins gyv. skaiciu po 15
 
 -- dazniausiai irasai daromi nurodant primary key (id)
+
+update city 
+set district = 'Marijampoles rajonas'
+where id = 5002
+
+-- su ijungtu Safe Updates (rejects UPDATEs and DELETEs with no restrictions) leidzia pakeitimus daryti nurodzius ID 
