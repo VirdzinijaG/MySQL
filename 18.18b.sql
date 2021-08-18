29047125d5f0e5ec192e86003d2adb9de1b9de54
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-18 21:34:10 +0300

    Update 18.18b.sql

--- 18.18b.sql
+++ 18.18b.sql
@@ -1,2 +1,9 @@
-insert into zmones (vardas, pavarde, gim_data, alga)
-values ('Petras', 'Petraitis', '1999-01-01',12345.85)
+-- insert into zmones (vardas, pavarde) kitos reiksmes bus null
+-- values ('Jonas', 'Jonaitis')
+
+-- insert into zmones (vardas, pavarde, gim_data, alga)
+-- values ('Petras', 'Petraitis', '1999-01-01',12345.85)
+
+insert into zmones (vardas, pavarde, gim_data)
+values ('Antanas', 'Antanaitis', '1985-05-15')
+
