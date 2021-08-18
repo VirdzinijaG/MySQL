358eddec2b3bf3bdb6e62702951c42e2635033c5
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-18 10:54:24 +0300

    Update 08.17a.sql

--- 08.17a.sql
+++ 08.17a.sql
@@ -1,7 +1,12 @@
--- insert into city pridedamas irasasa i city lentele
+-- insert into city pridedamas irasas i city lentele
 -- values (5000, 'Balbieriskis', 'LTU', 'Prienu', 966); 
 -- values turi buti surasytos tokia tvarka, kokia yra lenteleje 
 -- ID Name CountryCode District Population
 
+-- id susigeneruos automatiskai sekantis (auto increment)
+-- (nurodomi stulpeliai, ta tvarka, kuria nurodysim duomenys)
+-- insert into city (countrycode, district, name, population)
+-- values ('LTU', 'Jurbarko', 'Klausuciai', 902);
+
 insert into city (countrycode, district, name, population)
-values ('LTU', 'Jurbarko', 'Klausuciai', 902);
+values ('LTU', 'Marijampoles', 'Vilkaviskis', 9444)
