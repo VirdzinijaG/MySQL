9c8a534ba29f6cfa8628121b88f7086a371de51c
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-18 09:46:19 +0300

    Update 08.16nd.sql

--- 08.16nd.sql
+++ 08.16nd.sql
@@ -6,3 +6,10 @@ where countrylanguage.IsOfficial = 'T' and Country.Code = countrylanguage.Countr
 group by Country.Code, country.name, country.Continent
 having count(*) >= 2
 order by country.name
+
+-- select country.name as salies pavadinimas, country.Continent as kontinentas, country.code as salies kodas, countrrylanguage.isOfficial as oficiali kalba
+-- from country, countryLanguage
+-- where country.code = countrylanguage.countrycode and countrylanguage.IsOfficial = 'T'
+-- group by countryLanguage.countrycode, country.name, country.continent, country.code, countrylanguage.isOfficial
+-- having count(*) > 1
+-- order by country.name
