2c2240f6cbf61b12cfa18f90b9365676b46165d5
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-18 11:06:13 +0300

    Update 08.17c.sql

--- 08.17c.sql
+++ 08.17c.sql
@@ -1,7 +1,11 @@
 -- update city
 -- set population = 967, district = 'Prienu r.'
 -- where id = 5000;
+-- atnaujinamas vienas irasas pagal id
 
 update city
 set population = population + 15
 where countrycode = 'LTU';
+-- atnaujinami irasai pagal countryde ir visiems padidins gyv. skaiciu po 15
+
+-- dazniausiai irasai daromi nurodant primary key (id)
