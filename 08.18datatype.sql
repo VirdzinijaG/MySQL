32dd421320c1b797e0655b3f31863ade10ba5a5e
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-18 21:30:05 +0300

    Create 08.18datatype.sql

---
+++ 08.18datatype.sql
@@ -0,0 +1,21 @@
+-- standartiniai duomenu tipai (datatype)
+
+-- decimal - skaicius
+-- naudojant decimal () skliausteliuose nurodoma kiek skaitemnu bus naudojama, ir kiek skaiciu po kableliu
+-- decimal(10,3) - 1234567.123
+-- Int yra sutrumpinimas decimal(11,0)
+
+-- char - tekstui
+-- char(10) saugos 10 simboliu
+-- varchar - tekstui
+-- varchar(10) saugos tiek simboliu kiek yra ivesta
+-- char(max simboliu kiekis)
+-- varchar(max simboliu kiekis)
+-- max simboliu kiekis - 1 iki 255
+
+-- date - datai
+-- time - laikui
+-- datetime - ir datai ir laikui
+
+-- clob
+-- blob
