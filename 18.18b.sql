6795f81b4083c00cdd706df3510964c443b94e49
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-19 10:41:25 +0300

    Update 18.18b.sql

--- 18.18b.sql
+++ 18.18b.sql
@@ -4,6 +4,17 @@
 -- insert into zmones (vardas, pavarde, gim_data, alga)
 -- values ('Petras', 'Petraitis', '1999-01-01',12345.85)
 
-insert into zmones (vardas, pavarde, gim_data)
-values ('Antanas', 'Antanaitis', '1985-05-15')
+-- insert into zmones (vardas, pavarde, gim_data)
+-- values ('Antanas', 'Antanaitis', '1985-05-15')
+
+-- insert into kontaktai (tipas, reiksme)
+-- values ('adresas', 'Lauko g. 30')
+-- neives naujo iraso, nes zmones_id doesn't have a default value
+
+-- insert into kontaktai (zmones_id, tipas, reiksme)
+-- values (5, 'mob', 865428932)
+-- neives reiksmes, nes zmones lenteleje nera tokio id
+
+insert into kontaktai (zmones_id, tipas, reiksme)
+values (4, 'adresas', 'pievu g. 15')
 
