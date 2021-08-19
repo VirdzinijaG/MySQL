e346f97974497c1639248859e20a4ffcad1b3a64
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-19 12:47:08 +0300

    update

--- 08.18nd.sql
+++ 08.18nd.sql
@@ -13,5 +13,8 @@
 -- insert into apmokejimo_tipai (pavadinimas)
 -- values ('Pavedimu')
 
-insert into cekiai (data, pardavejai_id, apmokejimo_tipai_id)
-values ('2021-08-01', 1, 1)
+-- insert into cekiai (data, pardavejai_id, apmokejimo_tipai_id)
+-- values ('2021-08-15', 4, 3)
+
+insert into prekes (cekiai_id, pavadinimas, kaina, islaidu_tipai_id)
+values (3, 'Mineralinis', 1.36, 2)
--- 08.18nd1.sql
+++ 08.18nd1.sql
@@ -1,2 +1,6 @@
 select * 
-from pardavejai join cekiai on pardavejai.id = cekiai.pardavejai_id
+from pardavejai join cekiai on pardavejai.id = cekiai.pardavejai_id 
+join prekes on cekiai.id = prekes.cekiai_id
+join islaidu_tipai on islaidu_tipai.id = prekes.islaidu_tipai_id
+join apmokejimo_tipai on cekiai.apmokejimo_tipai_id = apmokejimo_tipai.id
+order by cekiai.data
