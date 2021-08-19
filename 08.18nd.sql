0a56d16a3d9f8dd17a890b53402cd6d98689458f
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-19 11:25:29 +0300

    Update 08.18nd.sql

--- 08.18nd.sql
+++ 08.18nd.sql
@@ -1,5 +1,8 @@
 -- insert into islaidu_tipai (pavadinimas)
 -- values ('maistas')
 
-insert into islaidu_tipai (pavadinimas)
-values ('gerimai')
+-- insert into islaidu_tipai (pavadinimas)
+-- values ('gerimai')
+
+insert into pardavejai (id, pavadinimas)
+values(1, 'Maxima')
